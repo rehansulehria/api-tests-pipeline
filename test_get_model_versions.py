85653c2c9af30afc666a40c14234e39dc5f7c752
Rehan Shahid <rehan.shahid@alefeducation.com>
2024-10-27 19:44:10 +0400

    add api tests

--- test_get_model_versions.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from src.tests.conftest import create_model
-
-@pytest.mark.order(4)
-def test_get_model_version(model_client, create_model, create_version):
-    model_id_value, name_value = create_model
-    response = model_client.get_model_versions(model_id_value)
--- test_getmodels.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.mark.order(5)
-def test_getModels(model_client):
-    response = model_client.get_models()
-    assert response is not None
-    assert type(response) == list
